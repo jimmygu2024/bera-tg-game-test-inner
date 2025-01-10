b42e2472d7472ce56a69fb53e4dd9ba31dfa6a70
jimmygu <jimmyguu2024@gmail.com>
2025-01-10 18:27:05 +0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import { useLoad } from "@/hooks/use-load";
 import { onCopy } from "@/utils";
 
 export default function Home() {
-  const [data, dataList] = useLoad();
+  const [dataList] = useLoad();
 
   return (
     <div className="h-full overflow-y-auto">
       <ul>
         {
-          dataList?.map((it: any, idx: number) => (
+          dataList?.map?.((it: any, idx: number) => (
             <li key={idx} className="flex gap-2">
               <label className="font-bold">{it.label}:</label>
               <div
--- src/hooks/use-load.ts
+++ src/hooks/use-load.ts
@@ -1,43 +1,58 @@
 import { useEffect, useState } from "react";
 
 export function useLoad() {
-  const [data, setData] = useState({});
   const [dataList, setDataList] = useState<any>();
+  const [WebApp, setWebApp] = useState<any | null>(null);
+  const [isInitialized, setIsInitialized] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     console.log('use load: %o', window);
 
-    if (!window || typeof window === 'undefined') return;
+    const initWebApp = async () => {
+      try {
+        const WebAppModule = await import("@twa-dev/sdk");
+        WebAppModule.default.ready();
+        WebAppModule.default.expand();
+        const _WebApp = WebAppModule.default;
+        setWebApp(_WebApp);
+        setIsInitialized(true);
 
-    const handleMessage = (e: any) => {
-      console.log('listened message: %o', e);
-      if (e.data?.type !== 'Beraciaga') return;
-      const _data = e.data.data;
-      setData(_data);
-
-      const _list: any = [];
-      const formatData = (it: any) => {
-        for (const key in it) {
-          if (typeof it[key] !== 'object') {
-            _list.push({
-              label: key,
-              value: it[key]?.toString(),
-            });
-            continue;
+        //#region test data
+        const _data = {
+          initData: _WebApp.initData,
+          initDataUnsafe: _WebApp.initDataUnsafe,
+          version: _WebApp.version,
+          viewportHeight: _WebApp.viewportHeight,
+          viewportStableHeight: _WebApp.viewportStableHeight,
+          isExpanded: _WebApp.isExpanded,
+          platform: _WebApp.platform,
+  
+          API: process.env.NEXT_PUBLIC_API,
+        };
+        const _list: any = [];
+        const formatData = (it: any) => {
+          for (const key in it) {
+            if (typeof it[key] !== 'object') {
+              _list.push({
+                label: key,
+                value: it[key]?.toString(),
+              });
+              continue;
+            }
+            formatData(it[key]);
           }
-          formatData(it[key]);
-        }
-      };
-      formatData(_data);
-      setDataList(_list);
+        };
+        formatData(_data);
+        setDataList(_list);
+        //#endregion
+      } catch (err) {
+        setError("Failed to load Telegram WebApp SDK");
+      }
     };
 
-    window.addEventListener('message', handleMessage);
-
-    return () => {
-      window.removeEventListener('message', handleMessage);
-    };
+    initWebApp();
   }, []);
 
-  return [data, dataList];
+  return [dataList, WebApp, isInitialized, error];
 }
